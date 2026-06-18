0d6f1b52a4ae86adcf1afd2dd66699060939769b
George Vieira <95360922+GeorgeVieiraNRB@users.noreply.github.com>
2026-06-18 00:14:17 -0300

    Add: longest-consecutive-sequence - submission-12

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-12.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let seq = new Map()
+        let bigSeq = []
+
+        if(nums.length <= 1) return nums.length
+
+        nums.forEach((value,index)=>{
+            if(!seq.has(value)){
+                seq.set(value,[value])
+            }
+        })
+        
+        let coef = 0
+        let i = 1 
+
+        nums = [...seq.keys()];
+
+        while(coef < nums.length){
+            let value = nums[coef]
+            let temp = []
+            if(seq.has(value-1) || !seq.has(value+i)){
+                coef+=1
+                i=1
+                continue
+            }
+            seq.get(value).push(value + i);
+            // temp = [...seq.get(value),value+i]
+            temp = seq.get(value)
+
+            i+=1
+            
+            if(bigSeq.length < temp.length ){
+                bigSeq = temp
+            }
+            console.log(bigSeq)
+        }
+        return bigSeq[0]!=bigSeq[bigSeq.length-1] ? bigSeq.length : 1
+    }
+}
