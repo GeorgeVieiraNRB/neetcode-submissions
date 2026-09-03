348c896ce518fc12eaf7d80085aaad1f46fa883c
George Vieira <95360922+GeorgeVieiraNRB@users.noreply.github.com>
2026-09-02 22:23:10 -0300

    Add: merge-two-binary-trees - submission-1

---
+++ Data Structures & Algorithms/merge-two-binary-trees/submission-1.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root1
+     * @param {TreeNode} root2
+     * @return {TreeNode}
+     */
+    mergeTrees(root1, root2) {
+        if (!root1) return root2;
+        if (!root2) return root1;
+               let t = new TreeNode((root1 ? root1.val : 0 ) + (root2 ? root2.val : 0 ))
+
+        t.left = this.mergeTrees((root1 ? root1.left : null ),(root2 ? root2.left : null ))
+        t.right = this.mergeTrees((root1 ? root1.right : null ),(root2 ? root2.right : null ))
+
+        return t
+
+    }
+}
