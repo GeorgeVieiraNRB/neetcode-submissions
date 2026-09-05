401df3aa9e64182af1486c68b9de776af52a5691
George Vieira <95360922+GeorgeVieiraNRB@users.noreply.github.com>
2026-09-04 22:04:24 -0300

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    constructor(temp = 0){
+        this.temp = temp
+    }
+
+    max(a,b){
+        return a>b?a:b
+    }
+
+    down(root){
+        if(!root){
+            return 0  
+        }
+        let l = this.down(root.left)
+        let r = this.down(root.right) 
+        this.temp = this.max(this.temp,(r+l))//
+        return this.max(r,l) + 1//
+    }
+
+    diameterOfBinaryTree(root) {
+        this.down(root)
+        return this.temp
+    }
+}
