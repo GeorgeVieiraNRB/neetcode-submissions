2803db0b2bd15ccf8a63b5191da21416f0274eef
George Vieira <95360922+GeorgeVieiraNRB@users.noreply.github.com>
2026-09-20 21:33:25 -0300

    Add: range-sum-of-bst - submission-0

---
+++ Data Structures & Algorithms/range-sum-of-bst/submission-0.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} low
+     * @param {number} high
+     * @return {number}
+     */
+    rangeSumBST(root, low, high) {
+        if(!root){
+            return 0
+        }
+        let val = low <= root.val && high >= root.val ? root.val : 0
+        return val + this.rangeSumBST(root.left,low, high) + this.rangeSumBST(root.right,low, high)
+    }
+}
